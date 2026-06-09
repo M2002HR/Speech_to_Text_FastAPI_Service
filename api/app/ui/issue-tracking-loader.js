2246f37c73464c42e345efe1b597e8ea6205ce4e
Mohammad Hossein Rezayati <119920149+M2002HR@users.noreply.github.com>
2026-06-09 16:55:09 +0330

    chore(realtime): remove temporary issue tracking loader

--- api/app/ui/issue-tracking-loader.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const href = '/ui-assets/issue-tracking.css';
-  if ([...document.styleSheets].some((sheet) => sheet.href && sheet.href.endsWith(href))) return;
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = href;
-  document.head.appendChild(link);
-})();
